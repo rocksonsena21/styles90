07dd451a34c5c096b7b689a19deea6a696daf237
Rockson sena <rocksonsena21@gmail.com>
2026-02-25 18:13:07 +0000

    Refactor installed apps and middleware; add CORS support for frontend URLs (REACT)

--- styles90/settings.py
+++ styles90/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'users',
     'products',
-    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'styles90.urls'
@@ -118,3 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+# Allow your frontend URL
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # React dev server
+    "https://90plusstyles.vercel.app",  # React production
+]
