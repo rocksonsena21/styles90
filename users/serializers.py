77b348e84435cf261aa4cfa9422d0397dd9cdf43
O6SPIDER <arnoldagyemang14@gmail.com>
2026-03-01 18:15:43 +0000

    signupactivation

--- users/serializers.py
+++ users/serializers.py
@@ -15,7 +15,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'password_confirm', 'first_name', 'last_name']
+        fields = ['username', 'email', 'password', 'password_confirm']
 
     def validate(self, data):
         if data['password'] != data['password_confirm']:
